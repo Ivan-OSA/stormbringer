7b53321bb8717bce1940b8c1a93afbf2a4380c11
Ivan-OSA <72036448+Ivan-OSA@users.noreply.github.com>
2020-09-28 20:55:53 +0300

    Stormbringers

---
+++ Stormbringers/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		slickArrow: true,
+		infinite:true,
+		autoplay:true,
+		autoplaySpeed:5000,
+		tuchTrashold:10,
+		tuchMove:true,
+		Speed: 2000,
+	});
+	$('.slidervid').slick({
+		slickArrow: true,
+		infinite:true,
+		autoplay:true,
+		autoplaySpeed:5000,
+		tuchTrashold:10,
+		tuchMove:true,
+		Speed: 2000,
+	});	
+});
+
